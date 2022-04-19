b582d664c1783f319cb9378610a230ff5827d281
Juan David Arce Martinez <juandavid.arce@utp.edu.co>
2022-04-19 17:54:14 -0500

    Permissions updated, not allowed update another users

--- ecomerce/users/views.py
+++ ecomerce/users/views.py
@@ -9,6 +9,11 @@ from rest_framework.response import Response
 from users.models import User
 
 
+# Permissions
+from rest_framework.permissions import AllowAny,IsAuthenticated
+from users.permissions import IsAccountOwner
+
+
 # Serializers
 from users.serializers import UserModelSerializer,UserSignupSerializer
 
@@ -22,6 +27,17 @@ class UserViewSet(mixins.RetrieveModelMixin,
     """
     queryset = User.objects.filter(is_active = True)
     serializer_class = UserModelSerializer
+
+    def get_permissions(self):
+        """Assign permissions based on action"""
+        if self.action in ['signup']:
+            permissions = [AllowAny()]
+        elif self.action in ['update']:
+            permissions = [IsAuthenticated(),IsAccountOwner()]
+        elif self.action in ['retrieve']:
+            permissions = [ IsAuthenticated()]
+        return permissions
+
 
     @action(methods = ["POST"],detail = False)
     def signup(self,request):
